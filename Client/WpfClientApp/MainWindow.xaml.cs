2a2ae4f039892f6bec76da0ee52a50ad2ce17798
Chris Lehrer <chrislehrer@icloud.com>
2022-04-19 22:22:48 +0100

    Added Client Init

--- Client/WpfClientApp/MainWindow.xaml.cs
+++ Client/WpfClientApp/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFDevTestExercise
+namespace WpfClientApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
